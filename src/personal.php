89a291cceb9a6a32857af63bd2a3c64ffecb0e22
simonpforster <simon.forster98@gmail.com>
2024-11-03 17:15:06 +0000

    2022

--- src/personal.php
+++ src/personal.php
@@ -63,7 +63,7 @@
             <div class="desktop-1-2 text">
                 <img path="/personal/2023/spectral-archive/archive.jpg" fix="width"
                      style="aspect-ratio: 1.15; object-fit: cover;">
-                <div>
+                <div class="text">
                     <div class="highlight">SPECTRAL ARCHIVE, 2023</div>
                     <div>
                         is about the process of memory reconstruction by bridging the gap between my digital and
@@ -75,23 +75,24 @@
                     </div>
                 </div>
             </div>
-
         </div>
         <div class="section-divider"></div>
         <div class="section" id="invisible-feast">
             <div class="normal-1-2 text">
                 <img path="/personal/2023/invisible-feast/web-index.png" fix="width"
                      style="">
-                <div class="highlight">INVISIBLE FEAST, 2023</div>
-                <div>
-                    discusses the diversity and possibilities of future dietary structures in the current
-                    context of the food crisis.
+                <div class="text">
+                    <div class="highlight">INVISIBLE FEAST, 2023</div>
+                    <div>
+                        discusses the diversity and possibilities of future dietary structures in the current
+                        context of the food crisis.
 
-                </div>
-                <div>
-                    by simulating a future in which extinct animals and sustainable protein sources are used as
-                    ingredients, efforts towards a “future-proof” diet are re-examined from an ironic
-                    perspective.
+                    </div>
+                    <div>
+                        by simulating a future in which extinct animals and sustainable protein sources are used as
+                        ingredients, efforts towards a “future-proof” diet are re-examined from an ironic
+                        perspective.
+                    </div>
                 </div>
             </div>
             <div class="normal-1-2">
@@ -123,9 +124,119 @@
                 <div class="highlight" style="margin-left: auto; horiz-align: right;">SLIDE →</div>
                 <img path="/personal/2023/invisible-feast/3-desert.png" fix="width">
             </div>
+        </div>
+        <div class="section-divider"></div>
+        <div class="section" id="the-visitor">
+            <div class="desktop-30">
+                <div class="gallery" id="the-visitor-gallery-1">
+                    <img style="display: block;"
+                         path="/personal/2022/the-visitor/drawing-board-7.jpg"
+                         fix="width">
+                    <img path="/personal/2022/the-visitor/drawing-board-6.jpg"
+                         fix="width">
+                    <img path="/personal/2022/the-visitor/drawing-board-5.jpg"
+                         fix="width">
+                    <img path="/personal/2022/the-visitor/drawing-board-4.jpg"
+                         fix="width">
+                    <img path="/personal/2022/the-visitor/drawing-board-3.jpg"
+                         fix="width">
+                    <img path="/personal/2022/the-visitor/drawing-board-2-2.jpg"
+                         fix="width">
+                </div>
+            </div>
+            <div class="desktop-1-2 text">
+                <div class="gallery" id="the-visitor-gallery-2">
+                    <img style="display: block;"
+                         path="/personal/2022/the-visitor/picture-booklet-1.jpg"
+                         fix="width">
+                    <img path="/personal/2022/the-visitor/picture-booklet-2.jpg"
+                         fix="width">
+                    <img path="/personal/2022/the-visitor/picture-booklet-3.jpg"
+                         fix="width">
+                </div>
+                <div class="text">
+                    <div class="highlight">THE VISITOR, 2022</div>
+                    <div>
+                        explores the hypothesis of a future where humans are able to travel back in time to change their
+                        regretful decisions as "visitors".
+                    </div>
+                    <div>
+                        five refractive mediums are used as carriers of transportation through the past and future to
+                        express my concerns about human’s self-destructive tendencies with metaphors: any attempt to
+                        change
+                        the past for self-protection will only lead to a more collapsed future.
+                    </div>
+                </div>
+            </div>
+            <div class="desktop-20 empty"></div>
+        </div>
+        <div class="section-divider"></div>
+        <div class="section" id="gutter-and-soul">
+            <div class="desktop-15 empty"></div>
+            <div class="desktop-35">
+                <div class="gallery shrink-right" id="gutter-and-soul-gallery">
+                    <img style="display: block;"
+                         path="/personal/2022/gutter-and-soul/phase-2-cemeteryland.jpg"
+                         fix="width">
+                    <img path="/personal/2022/gutter-and-soul/phase-1-make-a-wish.jpg"
+                         fix="width">
+                    <img path="/personal/2022/gutter-and-soul/phase-1-make-a-wish.jpg"
+                         fix="width">
+                </div>
+            </div>
+            <div class="desktop-1-2 text">
+                <img path="/personal/2022/gutter-and-soul/phase-3-the-dive.jpg"
+                     fix="width">
+                <div class="text">
+                    <div class="highlight">GUTTER AND SOUL, 2022</div>
+                    <div>
+                        explores the conception of “otherness”: what kind of life is accepted by the majority as
+                        satisfactory, and guaranteed to be free from defects and judgement.
+                    </div>
+                    <div>
+                        a series of surreal scenes and items are used to show the subtle poisoning and emasculation of
+                        the “minorities."
+                    </div>
+                </div>
             </div>
         </div>
+        <div class="section-divider"></div>
+        <div class="section" id="dialogue">
+            <div class="desktop-1-2 text">
+                <div class="gallery" id="dialogue-gallery-1">
+                    <img style="display: block;"
+                         path="/personal/2022/dialogue/alphabet.jpg"
+                         fix="width">
+                    <img path="/personal/2022/dialogue/poster-mockups.jpg"
+                         fix="width">
+                </div>
+                <div class="text">
+                    <div class="highlight">DIALOGUE, 2022</div>
+                    <div>
+                        discusses the association between attachment orientations and relationship dynamics in partners.
+                    </div>
+                    <div>
+                        with pursuit and distancing as the main theme, voices of couples with different attachment types
+                        in various scenarios are visualised as a typeface to highlight the relational basis of intimate
+                        violence.
+                    </div>
+                </div>
+            </div>
+            <div class="desktop-35">
+                <div class="gallery" id="dialogue-gallery-2">
+                    <img style="display: block;"
+                         path="/personal/2022/dialogue/installation-1.jpg"
+                         fix="width">
+                    <img path="/personal/2022/dialogue/installation-2.jpg"
+                         fix="width">
+                </div>
+            </div>
+            <div class="desktop-15 empty"></div>
+        </div>
+        <div class="section-divider"></div>
+
     </div>
+</div>
 </div>
 <?php include 'components/footer.php' ?>
 </body>
