baee3137390aabf225f8c8e8600afdc8044da655
Simon Forster <simon.forster98@gmail.com>
2024-11-26 17:59:05 +0000

    quick fixes

--- src/personal.php
+++ src/personal.php
@@ -29,7 +29,11 @@
 <div id="offscreen">
     <div class="column-container">
         <div class="section" id="rerendered-memory">
-            <div class="desktop-15 empty"></div>
+            <div class="desktop-25">
+                <img path="/personal/2024/rerendered-memory/combine-scan.jpg" fix="width">
+                <div class="spacing" style="height: 1.5rem;"></div>
+                <img path="/personal/2024/rerendered-memory/poster-picture-printer.jpg" fix="width">
+            </div>
             <div class="desktop-1-2 text">
                 <img path="/personal/2024/rerendered-memory/poster-scan.jpg" fix="width">
                 <div class="text">
@@ -39,11 +43,7 @@
                     </div>
                 </div>
             </div>
-            <div class="desktop-35">
-                <img path="/personal/2024/rerendered-memory/combine-scan.jpg" fix="width">
-                <div class="spacing" style="height: 1.5rem;"></div>
-                <img path="/personal/2024/rerendered-memory/poster-picture-printer.jpg" fix="width">
-            </div>
+            <div class="desktop-25 empty"></div>
         </div>
         <div class="section-divider"></div>
         <div class="section" id="spectral-archive">
