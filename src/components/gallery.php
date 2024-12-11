fe42f67801cfcdd9f5585616a2baaa031b5367ce
Simon Forster <simon.forster98@gmail.com>
2024-12-11 19:14:02 +0000

    add buttons to galleries

--- src/components/gallery.php
+++ src/components/gallery.php
@@ -1,12 +1,13 @@
 <?php
 
-
 function gallery(string $id, array $paths, bool $fix_width = true): void
 {
     $fix = $fix_width ? "width" : "height";
     echo '<div class="gallery" id="' . $id . '" fix="' . $fix . '">';
+    echo '<object class="arrow-left" data="./resources/icons/arrow.svg"></object>';
+    echo '<object class="arrow-right" data="./resources/icons/arrow.svg"></object>';
     foreach ($paths as $path => $fix) {
         echo '<img path="' . $path . '" fix="' . $fix . '"/>';
     }
-    echo '</div>';
+    echo "</div>";
 }
