438137303213e8bbaa34ba47e81a3a61ab8dcc83
simonpforster <simon.forster98@gmail.com>
2025-03-18 13:51:31 +0000

    query from projects store

--- src/router.py
+++ src/router.py
@@ -1,13 +1,31 @@
 from flask import Flask, render_template, redirect
+import logging
+from google.cloud import firestore
+
 import os
 
 app = Flask(__name__)
+
+base_logger = logging.getLogger("google")
+base_logger.addHandler(logging.StreamHandler())
+base_logger.setLevel(logging.DEBUG)
 
 image_resizer = os.getenv("IMAGE_RESIZER")
 pdf_portfolio = os.getenv("PDF_PORTFOLIO")
 
 app.jinja_env.globals['image_resizer'] = image_resizer
 app.jinja_env.globals['shop_open'] = os.getenv("SHOP_OPEN", "False")
+
+db = firestore.Client()
+
+collection_ref = db.collection('projects')
+query = collection_ref.where('owner', '==', 'Olivia Zuo')
+results = query.stream()
+
+# Print the results
+for doc in results:
+    print(f'{doc.id} => {doc.to_dict()}')
+
 
 @app.route("/", methods=['GET'])
 def index():
