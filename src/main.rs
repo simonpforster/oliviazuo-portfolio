ec1d3e9b81c03ea420ac7a9f59f615737f90711a
simonpforster <simon.forster98@gmail.com>
2025-04-08 15:06:05 +0100

    rip out router

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod components;
 pub mod observability;
 pub mod repository;
+mod router;
 
 use axum::{extract::State, http::StatusCode, middleware, response::{Html, IntoResponse}, routing::get, Router};
 use handlebars::Handlebars;
@@ -82,67 +83,11 @@ async fn main() {
     let state = Arc::new(AppState { hbs, image_resizer });
 
     // Set up our application with routes
-    let app = Router::new()
-        .route("/", get(index_handler))
-        .route("/personal", get(personal_handler))
-        .route("/commercial", get(commercial_handler))
-        .route("/portfolio.pdf", get(Redirect::permanent(&pdf_portfolio))) // to be turned into a proxy at a later date
-        .nest_service("/static", ServeDir::new("static"))
-        .with_state(state)
-        .layer(middleware::from_fn(extract_context));
-
+    let app: Router = router::router(pdf_portfolio, state);
     // Run our application
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     info!("listening on {}", addr);
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
-}
-
-#[instrument]
-async fn index_handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-    let data = json!({
-        "image_resizer": state.image_resizer,
-    });
-
-    match state.hbs.render("index", &data) {
-        Ok(html) => Html(html).into_response(),
-        Err(err) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Template error: {}", err),
-        )
-            .into_response(),
-    }
-}
-
-#[instrument]
-async fn personal_handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-    let data = json!({
-        "image_resizer": state.image_resizer,
-    });
-
-    match state.hbs.render("personal", &data) {
-        Ok(html) => Html(html).into_response(),
-        Err(err) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Template error: {}", err),
-        )
-            .into_response(),
-    }
-}
-
-#[instrument]
-async fn commercial_handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-    let data = json!({
-        "image_resizer": state.image_resizer,
-    });
-
-    match state.hbs.render("commercial", &data) {
-        Ok(html) => Html(html).into_response(),
-        Err(err) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Template error: {}", err),
-        )
-            .into_response(),
-    }
 }
