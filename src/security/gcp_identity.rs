745d1bfac08d664a9062dccd2a7e09af3f39bb14
Simon Forster <simon.forster98@gmail.com>
2025-05-25 11:01:21 +0100

    change audience based on env var (#80)
    use auth

--- src/security/gcp_identity.rs
+++ src/security/gcp_identity.rs
@@ -1,6 +1,6 @@
+use http::StatusCode;
 use std::process::Command;
 use std::sync::Arc;
-use http::StatusCode;
 use tracing::*;
 
 #[derive(Debug)]
@@ -9,20 +9,28 @@ pub struct IdentityProvider {
 }
 
 impl IdentityProvider {
-
     #[instrument]
-    pub async fn new() -> IdentityProvider {
+    pub async fn new(audience: &str) -> IdentityProvider {
         let reqwest_client = Arc::new(reqwest::Client::new());
 
+        let url = format!(
+            "http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience={}", 
+            audience
+        );
+
         let initial_token = match reqwest_client
-            .get("http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=https://portfolio-manager-test2-386756380530.europe-west1.run.app")
+            .get(url)
             .header("Metadata-Flavor", "Google")
             .send()
             .await
         {
             Ok(response) if response.status() == StatusCode::OK => response.text().await.unwrap(),
             Ok(response) => {
-                panic!("Error receiving the proper response from metadata server: {} {}", response.status(), response.text().await.unwrap());
+                panic!(
+                    "Error receiving the proper response from metadata server: {} {}",
+                    response.status(),
+                    response.text().await.unwrap()
+                );
             }
             Err(e) => {
                 warn!("falling back to env var, could not fetch id token: {}", e);
